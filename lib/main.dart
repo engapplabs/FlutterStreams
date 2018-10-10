0446e42540f64adbefbda177419cd8d4b3e031ed
Fellipe Malta <fellipe.dm@hotmail.com>
2018-10-10 20:29:54 -0300

    Using the .broadcast() method

--- lib/main.dart
+++ lib/main.dart
@@ -22,22 +22,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
-  ///1. Estamos criando nossa stream através do StreamController
+  ///Estamos criando nossa stream através do StreamController
   ///Uma classe que controla a stream que é criada
-  StreamController<String> streamController = new StreamController();
+  
+  ///Agora adicionamos o parametro ".broadcast()", como o nome já diz
+  ///serve para termos varias stream inscritas.
+  ///Sempre que precisamos ouvir algo da mesma stream em dois ou mais lugares diferentes, simultaneamente
+  ///Nós fazemos uso do ".broadcast()"
+  StreamController<String> streamController = new StreamController.broadcast();
+
   @override
   void initState() {
     super.initState();
 
     print("Creating a StreamController...");
+
+    //Primeira inscrição
     streamController.stream.listen((data) {
-      print("Data received: " + data);
+      print("Data received 1: " + data);
     }, onDone: () {
-      print("Task done");
-      //2. Perceba que o método "onDone" não foi chamado.
-      //Isso por que nossa stream está ativa e ainda não foi fechada
+      print("Task done 1");
     }, onError: (error) {
-      print("Some error");
+      print("Some error 1");
+    });
+
+    //Segunda inscrição
+    streamController.stream.listen((data) {
+      print("Data Received 2: " + data);
+    }, onDone: () {
+      print("Task done 2");
+    }, onError: (error) {
+      print("Some error 2");
     });
 
     streamController.add("This is a test data");
@@ -52,7 +67,7 @@ class HomeScreenState extends State<HomeScreen> {
     super.dispose();
   }
 
-  //OBS: O método ".listen()" retorna uma StreamSubscription". 
+  //OBS: O método ".listen()" retorna uma StreamSubscription".
   //A documentação diz que:
   //A inscrição fornece eventos ao ouvinte (listener), e segura o callback usado para
   //cuidar dos eventos. A inscrição também pode ser usada para se desinscrever de eventos
@@ -60,7 +75,6 @@ class HomeScreenState extends State<HomeScreen> {
 
   //Resumindo: A inscrição de uma stream pode tanto ser utilizada para pausar e resumir eventos de uma stream quando preciso.main(List<String> args) {
   //Sempre cancele a inscrição quando não for mais preciso o uso dela.
-  }
 
   Future<String> getData() async {
     await Future.delayed(Duration(seconds: 5));
